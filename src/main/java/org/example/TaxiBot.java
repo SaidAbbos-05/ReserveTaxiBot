f8e6c49998f619b0aa2db8ba98fe31250ccfe679
Your Name <you@example.com>
2024-11-20 15:22:30 +0500

    2-attempt

--- src/main/java/org/example/TaxiBot.java
+++ src/main/java/org/example/TaxiBot.java
@@ -4,13 +4,20 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 public class TaxiBot extends TelegramLongPollingBot {
+
+    public TaxiBot(){
+        super("7628684296:AAEW4cxbZ5EFUqjTASl_cyY5OcCwpUi8PrI");
+    }
+
     @Override
     public void onUpdateReceived(Update update) {
+
+
 
     }
 
     @Override
     public String getBotUsername() {
-        return "";
+        return "ReserveTaxi_bot";
     }
 }
